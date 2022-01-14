99657457f58e852775facddf563d8c88e1460a5a
Ralph Soika <ralph.soika@imixs.com>
2022-01-14 14:42:53 +0100

    docker config

--- jakarta9/buildAndRun.sh
+++ jakarta9/buildAndRun.sh
@@ -3,5 +3,6 @@ mvn clean package && docker build -t com.ralph.microprofile/hello-world .
 docker rm -f hello-world || true &&  \
 	docker run  \
 	-p 8080:8080 \
+	-p 9990:9990 \
 	-e MAIL_HOST="my.mail.host" \
 	--name hello-world com.ralph.microprofile/hello-world 
